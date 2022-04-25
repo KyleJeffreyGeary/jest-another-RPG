2a5095ffad2b51c4e43a030c15fbab72f377d982
KyleJeffreyGeary <gearykyle24@gmail.com>
2022-04-25 02:48:38 -0400

    current enemy and player properties

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -80,3 +80,17 @@ test('creates a player object', () => {
 
       expect(player.inventory.length).toBeLessThan(oldCount);
   });
+
+  test("subtracts from player's health", () => {
+    const player = new Player('Dave');
+    const oldHealth = player.health;
+  
+    player.reduceHealth(5);
+  
+    expect(player.health).toBe(oldHealth - 5);
+  
+    player.reduceHealth(99999);
+  
+    expect(player.health).toBe(0);
+  });
+  
